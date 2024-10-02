d193f436c90b47a517858ad348246ee38251a7d5
lengvietcuong <lengvietcuong@gmail.com>
2024-10-02 22:28:37 +0700

    fix background shift issue

--- components/squares-background.tsx
+++ components/squares-background.tsx
@@ -1,5 +1,5 @@
 export default function SquaresBackground() {
   return (
-    <div className="-z-20 bg-[url('/squares-background.svg')] max-w-screen max-h-screen bg-cover bg-center bg-no-repeat absolute inset-0 before:absolute before:inset-0 before:-z-10 before:bg-gradient-to-b before:from-transparent before:to-background"></div>
+    <div className="-z-20 bg-[url('/squares-background.svg')] max-w-[100svw] max-h-svh bg-cover bg-center bg-no-repeat absolute inset-0 before:absolute before:inset-0 before:-z-10 before:bg-gradient-to-b before:from-transparent before:to-background"></div>
   );
 }
