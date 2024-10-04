54c15e5425469032ac9e687954bd2752922033dc
katiue <vinhkhhangnguyen38@gmail.com>
2024-10-05 00:36:26 +0700

    added PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import withPWA from 'next-pwa';
+
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+export default withPWA({
+    dest: 'public',
+    skipWaiting: true,
+    register: true,
+    disable: process.env.NODE_ENV === 'development'
+})(nextConfig);
