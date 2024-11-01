3dee5a25cfc1937ac939021f7ecb577a4c1b54ff
lengvietcuong <lengvietcuong@gmail.com>
2024-11-01 13:02:36 +0700

    add chain analysis API

---
+++ chainAnalysis.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import axios from "axios";
+
+const URL = "https://public.chainalysis.com/api/v1/address";
+
+export async function getAddressReports(address: string) {
+  const response = await axios.get(`${URL}/${address}`, {
+    headers: {
+      'X-API-Key': `${process.env.CHAIN_ANALYSIS_API_KEY}`,
+      'Accept': "application/json",
+    },
+  });
+  return response.data;
+}
