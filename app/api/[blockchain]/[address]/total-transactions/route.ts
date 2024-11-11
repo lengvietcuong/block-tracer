7e8b776813fef0ffe7775f5c0a33cb97703668a3
~Sen~ <gahuyquang1@gmail.com>
2024-11-11 20:54:57 +0700

    update for swc

--- app/api/[blockchain]/[address]/total-transactions/route.ts
+++ app/api/[blockchain]/[address]/total-transactions/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
+import { initializeNeo4j, runQuery } from "@/lib/neo4j";
 import { BlockchainSymbol } from "@/types";
 import { COIN_NAMES, BIT_QUERY_URL } from "@/constants";
 
@@ -9,11 +10,36 @@ const headers = {
   "X-API-KEY": process.env.BIT_QUERY_API_KEY,
 };
 
+// Function to retrieve total transaction count from Neo4j for "swc"
+async function getSwincoinTotalTransactions(address: string) {
+  const neo4jDriver = initializeNeo4j();
+  const session = neo4jDriver.session();
+
+  try {
+    const query = `
+      MATCH (addr:Address {address: $address})-[:SENT|RECEIVED_BY]->(tx:Transaction)
+      RETURN COUNT(DISTINCT tx) AS totalTransactions
+    `;
+
+    const result = await runQuery(neo4jDriver, query, { address });
+    // console.log(result.records[0].get("totalTransactions").toNumber());
+    return result.records[0].get("totalTransactions").toNumber();
+  } finally {
+    await session.close();
+    await neo4jDriver.close();
+  }
+}
+
 export async function GET(
   _request: Request,
   { params }: { params: { blockchain: BlockchainSymbol; address: string } },
 ) {
   const { blockchain, address } = params;
+
+  if (blockchain === "swc") {
+    const totalCount = await getSwincoinTotalTransactions(address);
+    return NextResponse.json(totalCount);
+  }
 
   // Query to get total transaction count for an address (both sent and received)
   // Uses the 'any' operator to match transactions where the address is either sender or receiver
--- app/api/[blockchain]/transaction/[hash]/route.ts
+++ app/api/[blockchain]/transaction/[hash]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
+import { initializeNeo4j, runQuery } from "@/lib/neo4j";
 import { BlockchainSymbol } from "@/types";
 import { COIN_NAMES, BIT_QUERY_URL } from "@/constants";
 
@@ -9,11 +10,50 @@ const headers = {
   "X-API-KEY": process.env.BIT_QUERY_API_KEY,
 };
 
+// Function to retrieve total transaction count from Neo4j for "swc"
+export async function getSwincoinTransactionsDetails(hash: string) {
+  const neo4jDriver = initializeNeo4j();
+  const session = neo4jDriver.session();
+
+  try {
+    const query = `
+        MATCH (tx:Transaction {hash: $hash})
+        RETURN
+          tx.transaction_index AS index,
+          tx.gas AS gas,
+          tx.gas_used AS gasUsed,
+          tx.gas_price AS gasPrice,
+          tx.transaction_fee AS gasValue,
+          tx.block_number AS blockNumber,
+          tx.block_hash AS blockHash
+      `;
+
+    const result = await runQuery(neo4jDriver, query, { hash });
+
+    return {
+      transactionIndex: result.records[0].get("index").toNumber(),
+      gasUsed: result.records[0].get("gasUsed").toNumber(),
+      gasPrice: result.records[0].get("gasPrice").toNumber(),
+      transactionFee: result.records[0].get("gasValue").toNumber() / 1e18,
+      blockNumber: result.records[0].get("blockNumber").toNumber(),
+      blockHash: result.records[0].get("blockHash"),
+    };
+  } finally {
+    await session.close();
+    await neo4jDriver.close();
+  }
+}
+
 export async function GET(
   _request: Request,
   { params }: { params: { blockchain: BlockchainSymbol; hash: string } },
 ) {
   const { blockchain, hash } = params;
+
+  if (blockchain === "swc") {
+    const txnDetails = await getSwincoinTransactionsDetails(hash);
+    return NextResponse.json(txnDetails);
+  }
 
   // First query: Get transaction details using transaction hash
   let query = `
